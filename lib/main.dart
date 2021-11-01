276ab4b8221773c51ca3911754b86cd78b79774e
Ahmad Ali <92102583+Alikhichi15@users.noreply.github.com>
2021-11-01 11:28:43 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(QuizApp());
