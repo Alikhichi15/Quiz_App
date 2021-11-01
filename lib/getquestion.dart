24f458bfcedb2d450a1df07885ec6627df5158c3
Ahmad Ali <92102583+Alikhichi15@users.noreply.github.com>
2021-11-01 11:25:18 +0500

    Get Question
    method how to get question

---
+++ lib/getquestion.dart
@@ -0,0 +1,17 @@
+class Question {
+  String questiontext;
+  String optionA;
+  String optionB;
+  String optionC;
+  String optionD;
+  String correctAnser;
+  Question(String Q,String A,String B,String C,String D,String CA)
+  {
+    questiontext=Q;
+    optionA=A;
+    optionB=B;
+    optionC=C;
+    optionD=D;
+    correctAnser=CA;
+  }
+}
